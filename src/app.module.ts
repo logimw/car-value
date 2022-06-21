150eda908eef07ed7ec94afb02f32b961ff3acef
Mateusz <mateusz-wojcik@hotmail.com>
2022-06-21 22:36:26 +0200

    typeorm configuration for different environments and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,9 @@ import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
-import cookieSession = require('cookie-session');
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -16,23 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [User, Report],
-        };
-      },
-    }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot(),
     ReportsModule,
     UsersModule,
   ],
